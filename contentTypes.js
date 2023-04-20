c8435ffa880b800a84a987ba3bf45694035e7490
jannis <s0583823@htw-berlin.de>
2023-04-20 04:19:17 +0200

    first commit

---
+++ contentTypes.js
@@ -0,0 +1,14 @@
+module.exports = {
+    html: {
+        "Content-Type": "text/html"
+    },
+    js: {
+        "Content-Type": "text/js"
+    },
+    css: {
+        "Content-Type": "text/css"
+    },
+    text: {
+        "Content-Type": "text/plain"
+    }
+};
---
+++ utils.js
@@ -0,0 +1,15 @@
+const fs = require("fs"),
+httpStatus = require("http-status-codes"),
+contentTypes = require("contentTypes");
+
+module.exports = {
+    getFile: (file, res) => {
+        fs.readFile(`./${file}`, (error, data) => {
+            if(error){
+                res.writeHead(httpStatus.INTERNAL_SERVER_ERROR, contentTypes.html);
+                res.end("There was an error serving content!");
+            }
+            res.end(data);
+        });
+    }
+};
