9ae95d1316121c0003bbabd9181ed0a2188c5aa2
snahn2209 <sina.han@gmx.de>
2023-04-26 16:57:52 +0200

    added midleware functions
    noch nicht ganz fertog mit controller und routing (pg. 103)

---
+++ controllers/homeController.js
@@ -0,0 +1,2 @@
+const homeController = require("./controllers/homeController");
+//Move your route callback functions to the home controller, and add them to that module’s exports object.
--- main_express.js
+++ main_express.js
@@ -6,7 +6,31 @@ app = express();
 
 app.get("/", (req, res) => {
     res.send("hallo");
+    console.log(req.params); 
+    console.log(req.body); 
+    console.log(req.url); 
+    console.log(req.query);
 })
+//parameter -> localhost:3000/profile
+app.get("/:profile", (req, res) => {
+    let profile = req.params.profile;
+    res.send(`This is the page for your`+ profile);
+})
+
+app.use (
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
+
+//Capturing posted data from the request body in main.js
+app.post("/", (req, res) => {
+    console.log(req.body);
+    console.log(req.query); //to see query strings on server
+    res.send("POST Successful!");
+})
+
 .listen(port, () => {
     console.log('The Express.js server has started and is listening on port number:' + port);
 
