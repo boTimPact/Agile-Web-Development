9f031bcede8de313099c65585b974d7016c234e2
Jannis Suchanek <119596476+boTimPact@users.noreply.github.com>
2023-04-28 13:16:19 +0200

    refactored main.js so that middleware function logs data

--- main.js
+++ main.js
@@ -4,12 +4,24 @@ const port= 3000,
 express = require("express"),
 app = express();
 
+app.use(
+    express.urlencoded({
+        extended: false
+    }),
+    express.json()
+);
+
+app.use((req, res, next) => {
+    console.log("\n");
+    console.log(req.url);
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send("hallo");
-    console.log(req.params); 
-    console.log(req.body); 
-    console.log(req.url); 
-    console.log(req.query);
 })
 //parameter -> localhost:3000/profile
 app.get("/:profile", (req, res) => {
@@ -17,12 +29,6 @@ app.get("/:profile", (req, res) => {
     res.send(`This is the page for your`+ profile);
 })
 
-app.use (
-    express.urlencoded({
-        extended: false
-    })
-);
-app.use(express.json());
 
 //Capturing posted data from the request body in main.js
 app.post("/", (req, res) => {
