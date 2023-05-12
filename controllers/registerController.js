bf71313360a7d25812d59fef68f4aa25ebd3852c
Jannis Suchanek <119596476+boTimPact@users.noreply.github.com>
2023-05-12 02:26:55 +0200

    check for existing user

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -18,13 +18,24 @@ exports.signUpPost = (req, res) => {
     mongoose.connect("mongodb://91.58.14.60:27017/swappyDB", {useNewUrlParser: true});
     let db = mongoose.connection;
 
-    newUser.save()
-        .then(() => {
-            console.log("Success!")
+    let query = user.findOne({username: newUser.username});
+    query.exec()
+        .then((resDB) => {
+            console.log(resDB);
+            if(resDB != null){
+                res.send("Username already in use!");
+            }else{
+                newUser.save()
+                    .then(() => {
+                        console.log("Success!")
+                        res.redirect("./?user=" + newUser.username);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            }
         })
         .catch((err) => {
             console.log(err);
         });
-
-    res.redirect("./?user=" + newUser.username);
 }
