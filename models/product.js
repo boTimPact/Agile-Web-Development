e99a7ac5ede21072b5142ae2d6e1e863805e94b2
snahn2209 <sina.han@gmx.de>
2023-06-01 20:33:21 +0100

    added validations to models

--- models/product.js
+++ models/product.js
@@ -2,10 +2,16 @@ const mongoose = require("mongoose"),
         productSchema = mongoose.Schema({
                 product_id: String,
                 user_id: String,
-                title: String,
+                title: {
+                        type: String,
+                        required: true
+                },
                 description: String,
                 category: String,
                 size: String,
-                offer_type: String
+                offer_type: {
+                        type: String,
+                        required: true
+                }
         });
 module.exports = mongoose.model("Product", productSchema);
--- models/user.js
+++ models/user.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose"),
     userSchema = mongoose.Schema({
         user_id: String,
-        username: String,
-        password: String,
-        email: String,
-        address: String,
+        username: {
+            type: String,
+            required: true,
+            unique: [true, "This username is already in use!"]
+        },
+        password: {
+            type: String,
+            min: 4,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            unique: true
+        },
+        address: {
+            type: String
+        }
     });
 module.exports = mongoose.model("User", userSchema);
